f3940cd7a151bd128988f3e2b01b76432f0b25ff
khalid <achahbar061@gmail.com>
2023-03-14 11:30:08 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    print("1e Kotlin ")
+    println("oefen opdracht")
+    println("die ik maak.")
+
+    println(36 + 42)
+
+    val name = "Khalid Achahbar"
+    val birthyear = 2003
+
+    println(name)
+    println(birthyear)
+
+}
